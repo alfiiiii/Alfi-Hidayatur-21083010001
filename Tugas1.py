3df3aeb6ca44fbfaf210aa7304fdc8804776f072
alfiiiii <113012283+alfiiiii@users.noreply.github.com>
2022-09-18 20:10:21 +0700

    Add files via upload

---
+++ Tugas1.py
@@ -0,0 +1,10 @@
+print ('-' * 35)
+print ('Tugas 1 Sistem Operasi A083')
+
+print ('Nama : Alfi Hidayatur Rizki')
+print ('NPM  : 21083010001')
+
+print ('-' * 35)
+
+print ('Halo Dunia!')
+print ('Ini adalah script pertamaku di Linux!')
